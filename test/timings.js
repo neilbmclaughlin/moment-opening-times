148229a721bcd9f8d4523c0e13df15879fa50eea
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2016-11-05 19:19:18 +0000

    :bath: Clean data before using

--- test/timings.js
+++ test/timings.js
@@ -1,25 +1,44 @@
+const orgLimit = 1000;
+
 const OpeningTimes = require('../OpeningTimes');
-const orgs = require('./pharmacy-list');
+const orgs = require('./pharmacy-list').slice(0, orgLimit);
 const Moment = require('moment');
+
+const timeZone = 'Europe/London';
+const now = new Moment().tz(timeZone);
+
+function preProcessOrgs() {
+  orgs.forEach((org) => {
+    if (org.openingTimes) {
+      Object.keys(org.openingTimes.alterations).forEach((alteration) => {
+        if (new Moment(alteration).tz(timeZone).isBefore(now, 'day')) {
+          // console.log(alteration);
+          // eslint-disable-next-line no-param-reassign
+          delete org.openingTimes[alteration];
+        }
+      });
+    }
+  });
+}
 
 describe('timings', function () {
   this.timeout(3000);
-  describe('isOpen for 1000 orgs', () => {
-    const orgList = orgs.slice(0, 1000);
+  preProcessOrgs(orgs);
+  describe(`isOpen for ${orgs.length} orgs`, () => {
     it('disregarding alterations', () => {
-      orgList.forEach((o) => {
+      orgs.forEach((o) => {
         if (o.openingTimes) {
           const openingTimes =
-            new OpeningTimes(o.openingTimes.general, 'Europe/London');
+            new OpeningTimes(o.openingTimes.general, timeZone);
           openingTimes.isOpen(new Moment());
         }
       });
     });
     it('including alterations', () => {
-      orgList.forEach((o) => {
+      orgs.forEach((o) => {
         if (o.openingTimes) {
           const openingTimes =
-            new OpeningTimes(o.openingTimes.general, 'Europe/London', o.openingTimes.alterations);
+            new OpeningTimes(o.openingTimes.general, timeZone, o.openingTimes.alterations);
           openingTimes.isOpen(new Moment());
         }
       });
